8127048e14d0318b661c47b75e837e6c92192ab3
SRT-Epic <77328705+SRT-Epic@users.noreply.github.com>
2021-05-11 16:33:52 +0700

    Update Debris-Pseudo.lua

--- Debris-Pseudo.lua
+++ Debris-Pseudo.lua
@@ -1,18 +1,18 @@
 --Created by Wolfy#9349--
 --Version 1.0.0
-
 local Active = {}
 
 game:GetService("RunService").Stepped:Connect(function()
 	for Position, Info in pairs(Active) do
-		if os.clock() - Info[1] < Info[3] then return end
-		Info[2]:Destroy()
-		
+		if os.clock() - Info[1] < Info[2] then return end
+		Info[3]()
+
 		table.remove(Active, Position)
 	end
 end)
 
 return function(...)
+	if not ... then return end
 	local Time = os.clock()
 	table.insert(Active, {Time, ...})
 end
